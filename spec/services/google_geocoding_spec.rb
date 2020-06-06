b088196006054f80614de4cf7c63b6f852886599
David Tran <davidttemail@gmail.com>
2020-06-06 14:30:20 -0600

    Add test for Google's Geocoding

---
+++ spec/services/google_geocoding_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Google Geocoding" do
+  it "returns long/lat coords from a given address" do
+    url = "maps/api/geocode/json"
+    params = { address: "denver",
+               key: ENV['GEOCODE_KEY'] }
+    connection = Faraday.new("https://maps.googleapis.com/")
+    request = connection.get(url, params)
+    parsed = JSON.parse(request.body, symbolize_names: true)
+    expect(parsed[:status]).to eq("OK")
+    expect(parsed[:results]).to_not be_empty
+    expect(parsed[:results][0][:geometry]).to_not be_empty
+  end
+end
