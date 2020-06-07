ddf074c513c57d9810ef39d44c8ee14013f8ef81
David Tran <davidttemail@gmail.com>
2020-06-07 01:24:27 -0600

    Expand tests for returning forecast info

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 describe "Forecast requests" do
-  it "routes exist" do
-    get "/api/v1/forecast"
-  end
-
-  it "can take given params and request long/lat from Google" do
+  it "can take given location params and return forecast in json" do
     get "/api/v1/forecast?location=denver,co"
+
+    expect(response).to be_successful
+    forecast = JSON.parse(response.body, symbolize_names: true)
+    expect(forecast[:data][:type]).to eq("forecast")
+    expect(forecast[:data][:attributes][:hourly]).to_not be_empty
+    expect(forecast[:data][:attributes][:daily]).to_not be_empty
+    expect(forecast[:data][:attributes][:current]).to_not be_empty
+    expect(forecast[:data][:attributes][:geocode]).to have_key(:current_location)
   end
 end
