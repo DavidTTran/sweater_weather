10c9d6e4ba7413432ad8b58143421ce440853888
David Tran <davidttemail@gmail.com>
2020-06-08 00:30:58 -0600

    Add additional test

--- spec/requests/api/v1/new_user_spec.rb
+++ spec/requests/api/v1/new_user_spec.rb
@@ -12,20 +12,23 @@ describe "new user route" do
     expect(response).to be_successful
     expect(User.count).to eq(2)
 
-    parsed = JSON.parse(response.body)
+    parsed = JSON.parse(response.body, symbolize_names: true)
     expect(parsed[:data]).to_not be_empty
     expect(parsed[:data][:id]).to_not be_empty
     expect(parsed[:data][:attributes][:email]).to eq("whatever@example.com")
     expect(parsed[:data][:attributes][:api_key]).to_not be_empty
   end
 
-  it "can't post if email already exists" do
+  it "can't post if email already exists, or if passwords don't match" do
     create(:user, email: "123@example.com")
     params = { "email": "123@example.com",
                "password": "password",
-               "password_confirmation": "password" }
+               "password_confirmation": "wrong_password" }
 
     post "/api/v1/users", params: params
-    require "pry"; binding.pry
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed[:status]).to eq(400)
+    expect(parsed[:errors][0]).to eq("Email has already been taken")
+    expect(parsed[:errors][1]).to eq("Password confirmation doesn't match Password")
   end
 end
