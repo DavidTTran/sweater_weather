ce6177ecb8e3b7cf163ae8da94b7812dd0abfd0e
David Tran <davidttemail@gmail.com>
2020-06-08 11:51:22 -0600

    Test additional params

--- spec/requests/api/v1/foodie_spec.rb
+++ spec/requests/api/v1/foodie_spec.rb
@@ -5,10 +5,25 @@ describe "foodie endpoint" do
     get "/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian"
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(parsed[:data][:type]).to eq("foodie")
     expect(parsed[:data][:attributes]).to have_key(:end_location)
     expect(parsed[:data][:attributes][:end_location]).to eq("Pueblo, CO, USA")
+    expect(parsed[:data][:attributes]).to have_key(:travel_time)
+    expect(parsed[:data][:attributes]).to have_key(:forecast)
+    expect(parsed[:data][:attributes][:forecast]).to have_key(:summary)
+    expect(parsed[:data][:attributes][:forecast]).to have_key(:temperature)
+    expect(parsed[:data][:attributes][:restaurant]).to have_key(:name)
+    expect(parsed[:data][:attributes][:restaurant]).to have_key(:address)
+  end
+
+  it "different params" do
+    get "/api/v1/foodie?start=denver,co&end=orlando,fl&search=mexican"
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:data][:type]).to eq("foodie")
+    expect(parsed[:data][:attributes]).to have_key(:end_location)
     expect(parsed[:data][:attributes]).to have_key(:travel_time)
     expect(parsed[:data][:attributes]).to have_key(:forecast)
     expect(parsed[:data][:attributes][:forecast]).to have_key(:summary)
