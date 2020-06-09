0064edeb673c9c79a5724a5535e9dbdb90de4f15
David Tran <davidttemail@gmail.com>
2020-06-09 16:02:27 -0600

    Adjust tests for new poro

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -10,6 +10,20 @@ describe "Forecast requests" do
     expect(forecast[:data][:attributes][:hourly]).to_not be_empty
     expect(forecast[:data][:attributes][:daily]).to_not be_empty
     expect(forecast[:data][:attributes][:current]).to_not be_empty
-    expect(forecast[:data][:attributes][:geocode]).to have_key(:current_location)
+    expect(forecast[:data][:attributes][:location]).to_not be_empty
+    expect(forecast[:data][:attributes][:location][:location]).to eq("Denver, CO, USA")
+    forecast[:data][:attributes][:daily].each do |day|
+      expect(day).to have_key(:temp)
+      expect(day).to have_key(:weather)
+    end
+  end
+
+  it "returns an error if the location is wrong" do
+    get "/api/v1/forecast?location=qkwjeqle"
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:status]).to eq(200)
+    expect(parsed[:data][:results]).to eq([])
   end
 end
